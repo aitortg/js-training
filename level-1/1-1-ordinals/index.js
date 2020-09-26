7cf7ebe9e31931fc5ff10cfb2f34ca6e3f5a1bc7
Aitor Tusell <aitortg@gmail.com>
2020-09-27 00:16:36 +0200

    Resolucion del ejercicio 1

--- level-1/1-1-ordinals/index.js
+++ level-1/1-1-ordinals/index.js
@@ -1,7 +1,19 @@
-
 const numberToOrdinal = number => {
-  /* Your code here */
-  return "1st";
+  const lastTwoDigits = number % 100;
+  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13) {
+    return number + 'th';
+  }
+  const lastDigit = number % 10;
+  if (lastDigit === 1) {
+    return number + 'st';
+  }
+  if (lastDigit === 2) {
+    return number + 'nd';
+  }
+  if (lastDigit === 3) {
+    return number + 'rd';
+  }
+  return number + 'th';
 };
 
 module.exports = {
