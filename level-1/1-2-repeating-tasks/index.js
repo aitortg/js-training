f32dac148be8e73738603cd5cbb16ebf735b6d01
Aitor Tusell <aitortg@gmail.com>
2020-09-27 13:21:07 +0200

    Resolucion del ejercicio 2

--- level-1/1-2-repeating-tasks/index.js
+++ level-1/1-2-repeating-tasks/index.js
@@ -1,8 +1,27 @@
-
+/*Definición de la funcion*/
 const scheduleTasks = config => {
+
+  /*Se recogen los 3 valores del parametro "config"*/
   const { starting, repeat, each } = config;
-  /* Your code here */
-  return [];
+
+  /*Fecha que ira avanzando*/
+  const currentDate = new Date(starting);
+
+  /*Variable que retomaremos al final*/
+  const outputDate = [];
+
+  /*Se ejecuta el loop mientras coindida la condicion*/
+  while (outputDate.length < repeat) {
+    const weekday = currentDate.getDay();
+    if (weekday !== 0 && weekday !== 6) {
+      /*Version legible de la fecha*/
+      const readableDate = currentDate.toISOString().substring(0, 10);
+      outputDate.push(readableDate);
+    };
+    /*Sumo los dias "each" para la siguiete ejecución del loop while"*/
+    currentDate.setDate(currentDate.getDate() + each);
+  };
+  return outputDate;
 };
 
 module.exports = {
